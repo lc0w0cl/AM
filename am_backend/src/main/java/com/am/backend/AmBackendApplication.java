ea3cb24e7a30c676659023f8c3d4c07de5c282fc
lijiekang <middle_frist@163.com>
2024-11-06 17:19:01 +0800

    新建项目

---
+++ am_backend/src/main/java/com/am/backend/AmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.am.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmBackendApplication.class, args);
+    }
+
+}
